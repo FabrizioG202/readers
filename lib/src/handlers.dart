a20329743edb85d324f13a278a0dd31c0f0cfc61
Fabrizio Guidotti <fabrizio.guidotti.2000@gmail.com>
2024-10-26 15:37:59 +0200

    Modified handleSync method to support default request size and make it work with the new Request classes.

--- lib/src/handlers.dart
+++ lib/src/handlers.dart
@@ -8,34 +8,41 @@ typedef ManagedIncrementalParser<T> = Iterable<PartialParseResult<T>> Function(
 // API: Maybe add a default value to return in case of an exception.
 T handleSync<T>(
   ManagedIncrementalParser<T> createIterable,
-  SyncFileSource source,
-) {
+  SyncFileSource source, {
+  int defaultRequestSize = 2,
+}) {
   final buffer = BytesBuffer();
   final iterable = createIterable(buffer);
 
+  mainLoop:
   for (final partial in iterable) {
-    if (partial
-        case ReadRequest(
+    switch (partial) {
+      case ExactReadRequest(
           :final count,
-          :final position,
-          :final allowPartial,
-        )) {
-      final newData = source.read(count ?? 2, offset: position);
-      if (newData.isEmpty) break;
+          sourcePosition: final position,
+          bufferPosition: final bufferPosition
+        ):
+        final newData = source.read(count, offset: position);
+        if (newData.isEmpty || newData.length < count) break mainLoop;
 
-      // Not enough data.
-      // FIX: In theory, if allowPartial is not set, it does not make
-      // sense for the count to be null.
-      if (!allowPartial && count != null && newData.length < count) {
-        break;
-      }
+        buffer.grow(newData, position: bufferPosition);
 
-      buffer.grow(newData);
-    } else if (partial case CompleteParseResult<T>(:final value)) {
-      return value;
+      case PartialReadRequest(
+          :final maxCount,
+          sourcePosition: final position,
+          bufferPosition: final bufferPosition
+        ):
+        final requestSize = maxCount ?? defaultRequestSize;
+        final newData = source.read(requestSize, offset: position);
+        if (newData.isEmpty) break;
+
+        buffer.grow(newData, position: bufferPosition);
+
+      case CompleteParseResult<T>(:final value):
+        return value;
     }
   }
 
-  // FIXME: Choose wether to throw an exception or errors.
+  // EASY-FIXME: Choose wether to throw an exception or errors.
   throw Exception('Not enough data!');
 }
